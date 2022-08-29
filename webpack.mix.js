f5d361f9edcc2d7e98389a4d16938a7c2e8db3d7
samiel1410 <samiel_141099@hotmail.com>
2022-08-29 07:55:16 -0500

    prodcution

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,7 +15,3 @@ mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
 
 
-mix.js("resources/js/app.js", "public/js")
-    .postCss("resources/css/app.css", "public/css", [
-      require("tailwindcss"),
-    ]);   
